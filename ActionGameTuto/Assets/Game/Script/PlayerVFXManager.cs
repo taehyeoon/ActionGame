2c5704b7323b27f2a0c45f7bdff1551354b855c4
leetaehyeon <https://github.com/taehyeoon>
2022-12-23 18:03:41 +0900

    6. Creating The Player Footstep VFX 완료

---
+++ ActionGameTuto/Assets/Game/Script/PlayerVFXManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class PlayerVFXManager : MonoBehaviour
+{
+    public VisualEffect footStep;
+
+    public void Update_FootStep(bool state){
+        if (state)
+            footStep.Play();
+        else
+            footStep.Stop();
+    }
+}
